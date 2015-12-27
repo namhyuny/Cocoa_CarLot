b6acf51ed6fa74d475277982549ef01f17bcf892
김남현 <namhyun@me.com>
2015-12-27 14:02:59 +0900

    Initial Commit

---
+++ CarLot/Document.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  CarLot
+//
+//  Created by namhyun on 2015. 12. 27..
+//  Copyright © 2015년 2Fworkshop. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+
+@end
+
---
+++ CarLot/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CarLot
+//
+//  Created by namhyun on 2015. 12. 27..
+//  Copyright © 2015년 2Fworkshop. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
